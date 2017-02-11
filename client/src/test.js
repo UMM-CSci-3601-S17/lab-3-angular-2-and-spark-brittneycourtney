024ff29b9cd1c5b583d3e22e28d6c2dfe1302b3d
cookx876 <cookx876@morris.umn.edu>
2017-02-10 19:25:20 -0600

    clean up
    deleted unnecessary code, attempted to write a test

--- client/src/test.js
+++ client/src/test.js
@@ -1,1 +1,7 @@
 require('./test-loader.ts');
+
+// describe('testing things',function(){
+//     it('should return the correct numbner of todos', function(){
+//         expect(todos
+//     })
+// })
